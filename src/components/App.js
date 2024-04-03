9c722483f13de5a6b99baf2020d3e6f1de2d4433
akshayp-mangospring <akshayp@mangospring.com>
2024-04-03 20:48:01 +0530

    Add a basic React App with Bootstrap and SASS - Change the default port from 3000 to 5000

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import "../stylesheets/App.sass";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Hello, World!</h1>
+      <h1>This is gonna be a basic Chat App</h1>
+    </div>
+  );
+}
+
+export default App;
